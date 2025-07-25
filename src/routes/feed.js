0a042c64b78aa0ae753b7d31ae53b58c99d4535c
HarishBabuNimmagadda <75689990+HarishBabuNimmagadda@users.noreply.github.com>
2025-07-25 15:31:29 +0530

    update feed api

--- src/routes/feed.js
+++ src/routes/feed.js
@@ -1,24 +1,41 @@
 import express from "express";
 import userAuth from "../middleware/auth.js";
 import User from "../models/user.js";
+import Connection from "../models/connection.js";
 
 const feedRouter = express.Router();
 
 // get all users route
 feedRouter.get("/feed", userAuth, async (req, res) => {
     try {
-      const users = await User.find({});
+      const currentUserId = req.user._id;
       
-      // Remove passwords from all users for security
-      const usersResponse = users.map(user => {
-        const userObj = user.toObject();
-        delete userObj.password;
-        return userObj;
+      // Find all connections where current user is involved (as fromUser or toUser)
+      const existingConnections = await Connection.find({
+        $or: [
+          { fromUser: currentUserId },
+          { toUser: currentUserId }
+        ]
       });
+      
+      // Extract user IDs that are already connected
+      const connectedUserIds = existingConnections.map(connection => {
+        return connection.fromUser.toString() === currentUserId.toString() 
+          ? connection.toUser 
+          : connection.fromUser;
+      });
+      
+      // Add current user to excluded list
+      const excludedUserIds = [...connectedUserIds, currentUserId];
+      
+      // Find users excluding current user and already connected users, and exclude password
+      const users = await User.find({ 
+        _id: { $nin: excludedUserIds } 
+      }).select("-password");
       
       res.status(200).json({
         success: true,
-        users: usersResponse,
+        users: users,
       });
     } catch (error) {
       res.status(500).json({
